bf002148897cf00c5c826f024656939278aaff7a
xBiltTom <biltonjr@hotmail.com>
2025-10-17 10:04:03 -0500

    Actualizacion de rutas

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import Login from './components/Login'
-import PostulantesPage from './components/PostulantesPage'
-import ProtectedRoute from './components/ProtectedRoute'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route 
-          path="/postulantes" 
-          element={
-            <ProtectedRoute>
-              <PostulantesPage />
-            </ProtectedRoute>
-          } 
-        />
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="*" element={<Navigate to="/login" replace />} />
-      </Routes>
+    <BrowserRouter basename="/N8NDashboard">
+      <App />
     </BrowserRouter>
   </StrictMode>,
 )
