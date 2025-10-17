c3271d3e930e798733923995a1d8e6556f40e220
xBiltTom <biltonjr@hotmail.com>
2025-10-17 10:29:55 -0500

    Fix routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+import Login from './components/Login'
+import PostulantesPage from './components/PostulantesPage'
+import ProtectedRoute from './components/ProtectedRoute'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/N8NDashboard">
-      <App />
+    <BrowserRouter basename="/automatizacion-landing">
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route 
+          path="/postulantes" 
+          element={
+            <ProtectedRoute>
+              <PostulantesPage />
+            </ProtectedRoute>
+          } 
+        />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>,
 )
